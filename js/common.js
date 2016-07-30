53079bd4331dd5e4bb2a801fd868d7aafec80d5f
AleksBasko <aleks.spiegel@gmail.com>
2016-07-30 22:17:55 +0300

    Aleks commit

--- js/common.js
+++ js/common.js
@@ -1,228 +1,171 @@
 
+// window.onload = function () {
 
-//   button0.onclick = function() {
-//     deposit.value += 0;
-//  };
+//     // Обработчик клика по табам
+//     var newsTabMoscow = document.querySelector('.mainTag');
 
-//   button1.onclick = function() {
-//     deposit.value += 1;
-//  };
-//    button2.onclick = function() {
-//     deposit.value += 2;
-//  };
-//    button3.onclick = function() {
-//     deposit.value += 3;
-//  };
-//    button4.onclick = function() {
-//     deposit.value += 4;
-//  };
-//    button5.onclick = function() {
-//     deposit.value += 5;
-//  };
-//    button6.onclick = function() {
-//     deposit.value += 6;
-//  };
-//    button7.onclick = function() {
-//     deposit.value += 7;
-//  };
-//     button8.onclick = function() {
-//     deposit.value += 8;
-//  };
-//     button9.onclick = function() {
-//     deposit.value += parseInt(document.getElementById('button9').value);
-//  };
+//     var newsListMoscow = document.querySelector('.taghidden');
 
-//     button20.onclick = function() {
-//     deposit2.value += parseInt(document.getElementById('button20').value);
-//  };
-//      button21.onclick = function() {
-//     deposit2.value += parseInt(document.getElementById('button21').value);
-//  };
-//      button22.onclick = function() {
-//     deposit2.value += parseInt(document.getElementById('button22').value);
-//  };
-//      button23.onclick = function() {
-//     deposit2.value += parseInt(document.getElementById('button23').value);
-//  };
-//      button24.onclick = function() {
-//     deposit2.value += parseInt(document.getElementById('button24').value);
-//  };
-//      button25.onclick = function() {
-//     deposit2.value += parseInt(document.getElementById('button25').value);
-//  };
-//      button26.onclick = function() {
-//     deposit2.value += parseInt(document.getElementById('button26').value);
-//  };
-//      button27.onclick = function() {
-//     deposit2.value += parseInt(document.getElementById('button27').value);
-//  };
-//       button28.onclick = function() {
-//     deposit2.value += parseInt(document.getElementById('button28').value);
-//  };
-//       button29.onclick = function() {
-//     deposit2.value += parseInt(document.getElementById('button29').value);
-//  };
-
-
-
-
-
-// button223.onclick = function(){
- 
-// if (parseInt(document.getElementById('term').value) == 333) {
-//   results = parseInt(document.getElementById('deposit').value) + parseInt(document.getElementById('deposit2').value);
-//   document.getElementById('result').innerHTML = results;
-// }
-
-// else if (parseInt(document.getElementById('term').value) == 444) {
-//   results = parseInt(document.getElementById('deposit').value) - parseInt(document.getElementById('deposit2').value);
-//   document.getElementById('result').innerHTML = results;
-// }
-
-// else if (parseInt(document.getElementById('term').value) == 555) {
-//   results = parseInt(document.getElementById('deposit').value) / parseInt(document.getElementById('deposit2').value);
-//   document.getElementById('result').innerHTML = results;
-// }
-
-// else if (parseInt(document.getElementById('term').value) == 777) {
-//   results = parseInt(document.getElementById('deposit').value) * parseInt(document.getElementById('deposit2').value);
-//   document.getElementById('result').innerHTML = results;
-// }
-
-
-// };
-
-
-
-
-// buttonplus.onclick = function(){
-//   results = parseInt(document.getElementById('deposit').value) + parseInt(document.getElementById('deposit2').value);
-//   document.getElementById('result').innerHTML = results;
-// };
-
-
-
-
-
-
-// button223.onclick=function() {
-// var number, results;
-
-// term = parseInt(document.getElementById('percent').value);
-
-//   number = parseInt(document.getElementById('deposit').value);
-//   results = number *2;
-//   document.getElementById('result').innerHTML = results;
+//     newsTabMoscow.addEventListener('click', function () {
+//         if ('none' == newsListMoscow.style.display) {
+//             newsListMoscow.style.display = 'block';
+//             newsTabMoscow.style.marginBottom = '0';
+//             newsTabMoscow.style.borderRadius = '10px 10px 0 0';
+//             }
     
+//         else {
+//         	newsListMoscow.style.display = 'none';
+//             newsTabMoscow.style.marginBottom = '7px';
+//             newsTabMoscow.style.borderRadius = '10px';
+        	
+//         }
+//     });
 // };
 
 
+// changeSystem.onclick = function() {
+//   var a = parseInt(document.querySelector('.countAcd').value);
+//   var b = parseInt(document.querySelector('.countBcd').value);
+//   var results;
+//     results = (a.toString(b));
 
+// document.getElementById('res1').innerHTML = a;
+// document.getElementById('res12').innerHTML = b;
+// document.getElementById('res123').innerHTML = results;
 
+// document.getElementById('resultChange').style.display = "block";
 
-//   button0.onclick = function() {
-//     deposit.value += 0;
-//  };
-
-//   button1.onclick = function() {
-//     deposit.value += 1;
-//  };
-//    button2.onclick = function() {
-//     deposit.value += 2;
-//  };
-//    button3.onclick = function() {
-//     deposit.value += 3;
-//  };
-//    button4.onclick = function() {
-//     deposit.value += 4;
-//  };
-//    button5.onclick = function() {
-//     deposit.value += 5;
-//  };
-//    button6.onclick = function() {
-//     deposit.value += 6;
-//  };
-//    button7.onclick = function() {
-//     deposit.value += 7;
-//  };
-//     button8.onclick = function() {
-//     deposit.value += 8;
-//  };
-//     button9.onclick = function() {
-//     deposit.value += parseInt(document.getElementById('button9').value);
-//  };
-
-
-
-// plus.onclick = function(){
- 
-//  var firstfigure = parseInt(document.getElementById('deposit').value);
-
-//   var div = document.createElement('div');
-//   div.className = "success";
-//   div.innerHTML = firstfigure;
-
-// document.body.insertBefore(div, document.body.firstChild);
-//    //document.getElementById('result').innerHTML = results;
-
-// };
-
-// itog.onclick = function(){
- 
-//  var secondfigure = parseInt(document.getElementById('deposit').value);
-//  var firstfigure = parseInt(document.querySelector('.success'));
-
-// var results = +firstfigure + secondfigure;
-
-//   document.getElementById('result').innerHTML = results;
-
-// };
-
-
-//сколько процентов занимет число а от числа b
-// percent.onclick = function() {
-//   var a = parseInt(document.querySelector('.countA').value);
-//   var b = parseInt(document.querySelector('.countB').value);
-// var x, results;
-
-//   x = 100/(b/a);
-//   results=x;
-// document.getElementById('result').innerHTML = results + '%';
 // }
 
 
-//Найти число x если a % от этого числа равны b
-percent.onclick = function() {
-  var a = parseInt(document.querySelector('.countA').value);
-  var b = parseInt(document.querySelector('.countB').value);
-var x, results;
-
-  x = 100/a*b;
-  results=x;
-document.getElementById('result').innerHTML = results;
-}
 
 
-//чему будет равно число, если оно составляет a% от числа b
-percent.onclick = function() {
-  var a = parseInt(document.querySelector('.countA').value);
-  var b = parseInt(document.querySelector('.countB').value);
-var x, results;
-
-  x = a/100*b;
-  results=x;
-document.getElementById('result').innerHTML = results;
-}
-
-
-
-// насколько процентов число a меньше числа b 
-percent.onclick = function() {
-  var a = parseInt(document.querySelector('.countA').value);
-  var b = parseInt(document.querySelector('.countB').value);
+// функция возведения в степень.
+raisedPower.onclick = function() {
+  var a = parseInt(document.querySelector('.raisPowA').value);
+  var b = parseInt(document.querySelector('.raisPowB').value);
   var results;
+    results = Math.pow(a, b);
 
-  results = ((b - a)/b*100).toFixed(2);
+document.getElementById('raisPowA').innerHTML = a;
+document.getElementById('raisPowB').innerHTML = b;
+document.getElementById('resultRaisedPower').innerHTML = results;
 
-document.getElementById('result').innerHTML = results;
+document.getElementById('showResultRaisPow').style.display = "block";
+
 }
+
+// функция вычисления корня.
+calculateRoot.onclick = function() {
+  var a = parseInt(document.querySelector('.calcRootA').value);
+  var b = parseInt(document.querySelector('.calcRootB').value);
+  var results;
+    results = (Math.pow(Math.abs(b), 1/a)).toFixed(5);
+
+document.getElementById('rootA').innerHTML = a;
+document.getElementById('rootB').innerHTML = b;
+document.getElementById('resultRoot').innerHTML = results;
+
+document.getElementById('showResultCalcRoot').style.display = "block";
+
+}
+
+
+
+
+// выичсление размера депозита с 
+//месячной капитализацией с пополнением раз в три месяца
+countDeposit.onclick = function() {
+  var a = parseInt(document.getElementById('deposit').value);
+  var b = parseInt(document.getElementById('percent').value);
+  var c = parseInt(document.getElementById('term').value);
+  var d = parseInt(document.getElementById('refill').value);
+  var e = parseInt(document.getElementById('sumReplenish').value);
+  var f = parseInt(document.getElementById('currency').value);
+  var reinvest = document.getElementById('reinvestment').checked;  
+  var results, curr;
+
+//   if (d == 1) {
+//   for (var i = 0; i < (c*12); i++) {
+//       var y = ((a/100*b)/12);
+
+//       a += y
+
+//   }
+// } else {
+//     if (d == 2) {
+//       for (var i = 0; i < (c*12); i++) {
+//       var y = ((a/100*b)/12);
+
+//       a = a + y+e;
+
+// }
+// }
+
+if (d == 3) {
+          for (var i = 0; i < (c*12); i++) {
+      var y = ((a/100*b)/12);
+
+      if ((i%3) == 0) {
+        a = a + y + e;
+      }
+        else {
+          a = a + y;
+}
+
+ 
+}
+}
+
+
+
+if (d == 6) {
+          for (var i = 0; i < (c*12); i++) {
+      var y = ((a/100*b)/12);
+
+      if ((i%6) == 0) {
+        a = a + y + e;
+      }
+        else {
+          a = a + y;
+}
+
+ 
+}
+}
+
+if ( f == 1) {
+    curr = '  руб.'; 
+}
+
+else if ( f == 2) {
+    curr = ' $'; 
+}
+else if ( f == 3) {
+    curr = ' €'; 
+}
+else if ( f == 4) {
+    curr = ' £'; 
+}
+else if ( f == 5) {
+    curr = ' ¥'; 
+}
+
+
+
+
+    results = a;
+
+
+document.getElementById('result').innerHTML = results + curr;
+
+
+}
+
+
+
+
+
+if ( == '' &&  == '' &&  == '' &&  == '' && )
+
+    
